ed049e2363cc05e47c4b9efd7e9a66fc7c188581
Biryulin Dmitry <filin2hat@yandex.ru>
2023-07-04 16:27:54 +0300

    рефакторинг MainActivity.kt

--- app/src/main/java/com/biryulindevelop/redditron/presentation/MainActivity.kt
+++ app/src/main/java/com/biryulindevelop/redditron/presentation/MainActivity.kt
@@ -1,36 +1,52 @@
 package com.biryulindevelop.redditron.presentation
 
+import android.os.Build
 import android.os.Bundle
 import android.view.View
+import android.view.WindowInsetsController
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.ui.setupWithNavController
+import by.kirich1409.viewbindingdelegate.viewBinding
 import com.biryulindevelop.redditron.R
 import com.biryulindevelop.redditron.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import dagger.hilt.android.AndroidEntryPoint
-
 @AndroidEntryPoint
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(R.layout.activity_main) {
 
-    private lateinit var binding: ActivityMainBinding
+    private val binding by viewBinding(ActivityMainBinding::bind)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setupNavigation()
+    }
 
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-
+    private fun setupNavigation() {
         val navView = binding.bottomNavigationView
         val navController = findNavController(R.id.nav_host_fragment_activity_main)
         navView.setupWithNavController(navController)
 
         navController.addOnDestinationChangedListener { _, destination, _ ->
-            if (destination.id == R.id.navigation_onboarding || destination.id == R.id.navigation_auth) {
-                navView.visibility = View.GONE
-                window?.decorView?.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN
-                /**off.documentation offers to use this instead of deprecated:
-                 * WindowInsetsController.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE, which requires API 30+ */
-            } else navView.visibility = View.VISIBLE
+            updateUIBasedOnDestination(destination.id, navView)
+        }
+    }
+
+    private fun updateUIBasedOnDestination(destinationId: Int, navView: BottomNavigationView) {
+        if (destinationId == R.id.navigation_onboarding || destinationId == R.id.navigation_auth) {
+            navView.visibility = View.GONE
+            updateSystemUI()
+        } else {
+            navView.visibility = View.VISIBLE
+        }
+    }
+
+    private fun updateSystemUI() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            window?.insetsController?.hide(WindowInsetsController.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE)
+        } else {
+            @Suppress("DEPRECATION")
+            window?.decorView?.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN
         }
     }
 }
